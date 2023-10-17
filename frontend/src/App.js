531136576677b46b2b5a103220074836dfb46c50
Herbert Parasio <herbert.parasio@gmail.com>
2023-10-17 12:12:40 -0300

    Connection Front/Db Done. Get Data Working. Next Step - > Post

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,9 @@ import styled from "styled-components";
 import Form from "./components/Form.js";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Grid from "./components/Grid";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Container = styled.div`
   width: 100%;
@@ -16,12 +19,31 @@ const Container = styled.div`
 
 const Title = styled.h2``;
 
+
+
 function App() {
+  const [users, setUsers] = useState([]);
+  const [onEdit, setOnEdit] = useState(null);
+
+  const getUsers = async () => {
+    try {
+      const response = await axios.get("http://localhost:8800");
+      setUsers(response.data.sort((a, b) => (a.name > b.name ? 1 : -1)));
+    } catch (error) {
+      toast.error(error);
+    }
+  };
+
+    useEffect(() => {
+      getUsers();
+    }, [setUsers]);
+
   return (
     <>
       <Container>
         <Title>Users</Title>
         <Form />
+        <Grid users={users} />
       </Container>
       <ToastContainer autoClose={3000} position={toast.POSITION.BOTTOM_LEFT} />
       <GlobalStyle />
